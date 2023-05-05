c4991257f562903ecab14b5fd412634d6e94d2c8
fawad.ali <fawad.ali@appinSnap.com>
2023-05-05 17:24:53 +0500

    init

--- app/src/main/java/com/example/koindemo/MyApplication.kt
+++ app/src/main/java/com/example/koindemo/MyApplication.kt
@@ -3,6 +3,7 @@ package com.example.koindemo
 import android.app.Application
 import com.example.koindemo.di.appModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -11,6 +12,7 @@ class MyApplication: Application() {
         super.onCreate()
         //init the koin
         startKoin {
+            androidLogger()
             androidContext( this@MyApplication)
             modules(appModule)
         }
